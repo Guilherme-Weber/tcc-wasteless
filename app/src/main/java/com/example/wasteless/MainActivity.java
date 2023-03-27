860c8a57dd5d710b5352c1cdac3709a4bb1af3e1
FrancyaneViana <francyane@live.com>
2023-03-27 20:14:38 -0300

    teste github

--- app/src/main/java/com/example/wasteless/MainActivity.java
+++ app/src/main/java/com/example/wasteless/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //teste github
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
