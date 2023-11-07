f7b05b92958b00bb1776d77e97be8aa6d84fe842
Guilherme-Weber <guilweber13@gmail.com>
2023-11-07 15:28:49 -0300

    Tela Login não funcional verção teste

--- app/src/main/java/com/guilhermeweber/wasteless/activity/activity/AutentificacaoActivity.java
+++ app/src/main/java/com/guilhermeweber/wasteless/activity/activity/AutentificacaoActivity.java
@@ -28,7 +28,7 @@ import com.guilhermeweber.wasteless.activity.model.Usuario;
 
 public class AutentificacaoActivity extends AppCompatActivity {
 
-    private Button botaoAcessar;
+    private Button botaoAcessar, buttonCadastrar;
     private EditText campoEmail, campoSenha, campoNome;
     private Switch tipoAcesso, tipoUsuario;
     private LinearLayout linearTipoUsuario;
@@ -64,20 +64,29 @@ public class AutentificacaoActivity extends AppCompatActivity {
         //verifica o usuario logado
 
 
-        tipoAcesso.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//        tipoAcesso.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) { // se estiver selecionado é uma empresa
+//                    botaoAcessar.setText("Cadastrar");
+//                    linearTipoUsuario.setVisibility(View.VISIBLE);
+//                    campoNome.setVisibility(View.VISIBLE);
+//                } else { // se n estiver selecionado é um usuario
+//                    botaoAcessar.setText("Acessar");
+//                    linearTipoUsuario.setVisibility(View.GONE);
+//                    campoNome.setVisibility(View.GONE);
+//                }
+//            }
+//        });
+
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) { // se estiver selecionado é uma empresa
-                    botaoAcessar.setText("Cadastrar");
-                    linearTipoUsuario.setVisibility(View.VISIBLE);
-                    campoNome.setVisibility(View.VISIBLE);
-                } else { // se n estiver selecionado é um usuario
-                    botaoAcessar.setText("Acessar");
-                    linearTipoUsuario.setVisibility(View.GONE);
-                    campoNome.setVisibility(View.GONE);
-                }
+            public void onClick(View v) {
+                startActivity(new Intent(AutentificacaoActivity.this, CadastrosUsuariosActivity.class));
             }
         });
+
 
         botaoAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,7 +96,7 @@ public class AutentificacaoActivity extends AppCompatActivity {
                 String senha = campoSenha.getText().toString();
                 String nome = campoNome.getText().toString();
 
-                usuario.setNome(nome);
+//                usuario.setNome(nome);
                 usuario.setEmail(email);
                 usuario.setSenha(senha);
                 usuario.setTipo(verificaUsuario());
@@ -97,83 +106,83 @@ public class AutentificacaoActivity extends AppCompatActivity {
                     if (!senha.isEmpty()) {
 
                         //Verifica se o switch esta em login ou cadastro
-                        if (tipoAcesso.isChecked()) { //cadastro
+//                        if (tipoAcesso.isChecked()) { //cadastro
+//
+//                            auth.createUserWithEmailAndPassword( // envia pro servidor o email e senha pra cadastro
+//                                    usuario.getEmail(), usuario.getSenha()
+//                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<AuthResult> task) {
+//
+//                                    if (task.isSuccessful()) { //Verifica se o processo de cadastro do usuario deu certo
+//
+//                                        String idUsuario = task.getResult().getUser().getUid();
+//                                        usuario.setId(idUsuario);
+//                                        usuario.salvar();
+//
+//                                        //usuario.updateUserName(usuario.getNome());
+//
+//                                        if (usuario.getTipo() == "E") {//Empresa
+//                                            startActivity(new Intent(getApplicationContext(), EmpresaActivity.class));
+//                                            finish();
+//                                            Toast.makeText(AutentificacaoActivity.this, "Empresa Cadastrada com Sucesso! ", Toast.LENGTH_LONG).show();
+//                                        } else {//usuario
+//                                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+//                                            finish();
+//                                            Toast.makeText(AutentificacaoActivity.this, "Usuario Cadastrado com Sucesso! ", Toast.LENGTH_LONG).show();
+//                                        }
+//                                    } else {
+//
+//                                        //caso cadastro n seja realizado com sucesso é mostrado uma mensagem de erro
+//                                        String erroExcecao = "";
+//                                        try {
+//                                            throw task.getException();
+//                                        } catch (FirebaseAuthWeakPasswordException e) {
+//                                            erroExcecao = "Digite uma senha mais forte!";
+//                                        } catch (FirebaseAuthInvalidCredentialsException e) {
+//                                            erroExcecao = "Por Favor, digite um e-mail válido";
+//                                        } catch (FirebaseAuthUserCollisionException e) {
+//                                            erroExcecao = "Esta conta já foi cadastrada";
+//                                        } catch (Exception e) {
+//                                            erroExcecao = "Ao Cadastrar usuário: " + e.getMessage();
+//                                            e.printStackTrace();
+//                                        }
+//                                        Toast.makeText(AutentificacaoActivity.this, "Erro:" + erroExcecao, Toast.LENGTH_LONG).show();
+//                                    }
+//                                }
+//                            });
+//
+//                        } else { //login
 
-                            auth.createUserWithEmailAndPassword( // envia pro servidor o email e senha pra cadastro
-                                    usuario.getEmail(), usuario.getSenha()
-                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
+                        auth.signInWithEmailAndPassword( // envia pro servidor email e senha pra logar
+                                email, senha
+                        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) { // verifica se o retorno foi um sucesso
 
-                                    if (task.isSuccessful()) { //Verifica se o processo de cadastro do usuario deu certo
+                                    usuario.redirectUser(AutentificacaoActivity.this);
+                                    Toast.makeText(AutentificacaoActivity.this, "Logado com Sucesso", Toast.LENGTH_SHORT).show();
 
-                                        String idUsuario = task.getResult().getUser().getUid();
-                                        usuario.setId(idUsuario);
-                                        usuario.salvar();
+                                } else {// caso login tenha dado falha ele informa ao usuario o erro
 
-                                        //usuario.updateUserName(usuario.getNome());
-
-                                        if (usuario.getTipo() == "E") {//Empresa
-                                            startActivity(new Intent(getApplicationContext(), EmpresaActivity.class));
-                                            finish();
-                                            Toast.makeText(AutentificacaoActivity.this, "Empresa Cadastrada com Sucesso! ", Toast.LENGTH_LONG).show();
-                                        } else {//usuario
-                                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                                            finish();
-                                            Toast.makeText(AutentificacaoActivity.this, "Usuario Cadastrado com Sucesso! ", Toast.LENGTH_LONG).show();
-                                        }
-                                    } else {
-
-                                        //caso cadastro n seja realizado com sucesso é mostrado uma mensagem de erro
-                                        String erroExcecao = "";
-                                        try {
-                                            throw task.getException();
-                                        } catch (FirebaseAuthWeakPasswordException e) {
-                                            erroExcecao = "Digite uma senha mais forte!";
-                                        } catch (FirebaseAuthInvalidCredentialsException e) {
-                                            erroExcecao = "Por Favor, digite um e-mail válido";
-                                        } catch (FirebaseAuthUserCollisionException e) {
-                                            erroExcecao = "Esta conta já foi cadastrada";
-                                        } catch (Exception e) {
-                                            erroExcecao = "Ao Cadastrar usuário: " + e.getMessage();
-                                            e.printStackTrace();
-                                        }
-                                        Toast.makeText(AutentificacaoActivity.this, "Erro:" + erroExcecao, Toast.LENGTH_LONG).show();
+                                    String excecao = "";
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthInvalidUserException e) {
+                                        excecao = "Usuario não está cadastrado";
+                                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                                        excecao = "E-mail e senha não são validos";
+                                    } catch (Exception e) {
+                                        excecao = "erro ao cadastrar usuário" + e.getMessage();
+                                        e.printStackTrace();
                                     }
+                                    Toast.makeText(AutentificacaoActivity.this, excecao, Toast.LENGTH_LONG).show();
+
                                 }
-                            });
-
-                        } else { //login
-
-                            auth.signInWithEmailAndPassword( // envia pro servidor email e senha pra logar
-                                    email, senha
-                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) { // verifica se o retorno foi um sucesso
-
-                                        usuario.redirectUser(AutentificacaoActivity.this);
-                                        Toast.makeText(AutentificacaoActivity.this, "Logado com Sucesso", Toast.LENGTH_SHORT).show();
-
-                                    } else {// caso login tenha dado falha ele informa ao usuario o erro
-
-                                        String excecao = "";
-                                        try {
-                                            throw task.getException();
-                                        } catch (FirebaseAuthInvalidUserException e) {
-                                            excecao = "Usuario não está cadastrado";
-                                        } catch (FirebaseAuthInvalidCredentialsException e) {
-                                            excecao = "E-mail e senha não são validos";
-                                        } catch (Exception e) {
-                                            excecao = "erro ao cadastrar usuário" + e.getMessage();
-                                            e.printStackTrace();
-                                        }
-                                        Toast.makeText(AutentificacaoActivity.this, excecao, Toast.LENGTH_LONG).show();
-
-                                    }
-                                }
-                            });
-                        }
+                            }
+                        });
+//                        }
                     } else {
                         Toast.makeText(AutentificacaoActivity.this, "Preencha o Senha!", Toast.LENGTH_SHORT).show();
                     }
@@ -185,13 +194,14 @@ public class AutentificacaoActivity extends AppCompatActivity {
     }
 
     private void inicializarComponentes() {
-        campoNome = findViewById(R.id.editTextNome);
-        campoEmail = findViewById(R.id.TextNomeCadastro);
-        campoSenha = findViewById(R.id.editTextEmpresaCategoria);
-        tipoAcesso = findViewById(R.id.switchTipoAcesso);
-        tipoUsuario = findViewById(R.id.switchTipoUsuario);
+//        campoNome = findViewById(R.id.editTextNome);
+        campoEmail = findViewById(R.id.editTextEmailLogin);
+        campoSenha = findViewById(R.id.editTextSenhaLogin);
+//        tipoAcesso = findViewById(R.id.switchTipoAcesso);
+//        tipoUsuario = findViewById(R.id.switchTipoUsuario);
         botaoAcessar = findViewById(R.id.buttonCadastro);
-        linearTipoUsuario = findViewById(R.id.linearTipoUsuario);
+        buttonCadastrar = findViewById(R.id.buttonCadastrar);
+//        linearTipoUsuario = findViewById(R.id.linearTipoUsuario);
     }
 
     public String verificaUsuario() {
--- app/src/main/java/com/guilhermeweber/wasteless/activity/activity/CadastrosUsuariosActivity.java
+++ app/src/main/java/com/guilhermeweber/wasteless/activity/activity/CadastrosUsuariosActivity.java
@@ -8,12 +8,11 @@ import android.widget.RadioButton;
 
 import com.guilhermeweber.wasteless.R;
 
-import org.w3c.dom.Text;
 
 public class CadastrosUsuariosActivity extends AppCompatActivity {
 
     //Instanciando RadioButton e textos para o tipo de cadastro que está sendo realizado
-    private Text nome, email, senha;
+    private String nome, email, senha;
     private RadioButton cliente, empresa;
 
     @SuppressLint({"MissingInflatedId", "WrongViewCast"})
@@ -22,18 +21,18 @@ public class CadastrosUsuariosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastros_usuarios);
 
-        nome = findViewById(R.id.TextNomeCadastro);
-        email = findViewById(R.id.TextEmailAddress);
-        senha = findViewById(R.id.TextSenhaCadastro);
+        nome = String.valueOf(findViewById(R.id.TextNomeCadastro));
+        email = String.valueOf(findViewById(R.id.TextEmailAddress));
+        senha = String.valueOf(findViewById(R.id.TextSenhaCadastro));
         cliente = findViewById(R.id.radioButtonClienteCadastro);
         empresa = findViewById(R.id.radioButtonEmpresaCadastro);
 
     }
 
     //Metódo que chama o radiobutton do tipo de cadastro
-    public void radiobutton(){
-        if(cliente.isChecked()) {
-        }else if(cliente.isChecked()) {
-        }
+    public void radiobutton() {
+        if (cliente.isChecked()) {
+        } else if (cliente.isChecked()) {
         }
     }
+}
