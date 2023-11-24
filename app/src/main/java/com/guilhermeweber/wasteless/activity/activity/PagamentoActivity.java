c64d2075ee366d631c2ecc223a441cac5103052b
FrancyaneViana <128006023+FrancyaneViana@users.noreply.github.com>
2023-11-24 13:43:44 -0300

    Lógica de pagamento na classe PagamentoActivity

--- app/src/main/java/com/guilhermeweber/wasteless/activity/activity/PagamentoActivity.java
+++ app/src/main/java/com/guilhermeweber/wasteless/activity/activity/PagamentoActivity.java
@@ -1,14 +1,17 @@
 package com.guilhermeweber.wasteless.activity.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.guilhermeweber.wasteless.R;
 
@@ -52,55 +55,60 @@ public class PagamentoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Lógica de pagamento aqui
+                if (realizarPagamento()) {
+                    mostrarAlertaPedidoConfirmado();
+                }
+            }
+        });
+    }
 
-                Intent intent = new Intent(PagamentoActivity.this, PedidoConfirmadoActivity.class);
+    private boolean realizarPagamento() {
+        if (radioGroupMetodoPagamento.getCheckedRadioButtonId() == -1) {
+            exibirMensagemErro("Selecione um método de pagamento");
+            return false;
+        }
+
+        if (radioGroupMetodoPagamento.getCheckedRadioButtonId() == R.id.radioButtonCredito
+                || radioGroupMetodoPagamento.getCheckedRadioButtonId() == R.id.radioButtonDebito) {
+            // Se o pagamento for cartão de crédito ou débito, validar os campos do cartão
+            String numeroCartao = textNumeroCartao.getText().toString().trim();
+            String dataExpiracao = textDataExpiracao.getText().toString().trim();
+            String cvv = textCVV.getText().toString().trim();
+
+            if (TextUtils.isEmpty(numeroCartao) || TextUtils.isEmpty(dataExpiracao) || TextUtils.isEmpty(cvv)) {
+                exibirMensagemErro("Preencha todos os campos do cartão");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void exibirMensagemErro(String mensagem) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Erro no Pagamento");
+        builder.setMessage(mensagem);
+        builder.setPositiveButton("OK", null);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void mostrarAlertaPedidoConfirmado() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pedido Confirmado");
+        builder.setMessage("Seu pedido foi confirmado com sucesso!");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Fecha o alerta e retorna para a tela de histórico de pedidos
+                Intent intent = new Intent(PagamentoActivity.this, HistoricoPedidosActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
-            }
-        });
-/*
-        // Botão para pagar com cartão de crédito
-        Button buttonCredito = findViewById(R.id.buttonCredito);
-        buttonCredito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Lógica para pagamento com cartão de crédito
-                realizarPagamento("Cartão de Crédito");
+                finish();
             }
         });
 
-        // Botão para pagar com Débito
-        Button buttonDebito = findViewById(R.id.buttonDebito);
-        buttonDebito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Lógica para pagamento com débito
-                realizarPagamento("Débito");
-            }
-        });
-
-        // Botão para pagar com Pix
-        Button buttonPix = findViewById(R.id.buttonPix);
-        buttonPix.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Lógica de pagamento com Pix aqui
-                realizarPagamento("Pix");
-            }
-        });
-    }
-
- */
-/*
-    // Método simulando a lógica de pagamento
-    private void realizarPagamento(String metodoPagamento) {
-
-        // Mensagem somente indicando o método de pagamento escolhido
-        String mensagem = "Pagamento realizado com sucesso usando " + metodoPagamento;
-        Intent intent = new Intent(PagamentoActivity.this, PedidoConfirmadoActivity.class);
-        intent.putExtra("mensagem", mensagem);
-        startActivity(intent);
-    }
-
- */
+        // Cria e exibe o AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
